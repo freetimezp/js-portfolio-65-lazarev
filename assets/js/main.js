4b74ef270ac97df5312d2dda9d3a916a7cabbd84
freetimezp <kraftukrnet@ukr.net>
2024-03-30 20:13:41 +0200

    06. js-portfolio-65-lazarev. Page2 section. Hover animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,34 @@ function navAnimation() {
 
 navAnimation();
 
+//hover animation
+function page2Animation() {
+    var relems = document.querySelectorAll(".right-elem");
+
+    relems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            });
+        });
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            });
+        });
+
+        elem.addEventListener("mousemove", function (dets) {
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 50,
+                y: dets.y - elem.getBoundingClientRect().y - 150,
+            });
+        });
+    });
+}
+
+page2Animation();
 
 
 
